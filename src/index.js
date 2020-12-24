9142bb00ebe235bfd73b46de0a599eb9c59add17
qiuchiyi <qiuchiyi@baidu.com>
2020-12-24 17:19:56 +0800

    source code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+function component() {
+    var    element = document.createElement('div');
+  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+
+
+        }
+
+const a = 1;
+let b  = 2;
+console.log(a == b)
+  
+  document.body.appendChild(component());
